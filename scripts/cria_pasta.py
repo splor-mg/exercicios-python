67bb404fef2c2117729857fa7e2f0c7629141c4d
Raiane Cardoso <166438302+raianecardoso@users.noreply.github.com>
2025-12-01 22:19:50 +0000

    feat(scripts): add script cria_pasta.py
    cria estrutura de pastas de exercícios e seus testes
    
    Fix #2

--- scripts/cria_pasta.py
+++ scripts/cria_pasta.py
@@ -5,10 +5,19 @@ parser = argparse.ArgumentParser()
 
 
 parser.add_argument("caminho_pasta")
-parser.add_argument("nome_arquivo")
 args = parser.parse_args()
-print(args.nome_arquivo, args.caminho_pasta)
+# print(args.nome_arquivo, args.caminho_pasta)
 
 caminho_pasta = Path(args.caminho_pasta)
-if not caminho_pasta.is_dir(): 
-    print(f'{caminho_pasta} não é um caminho válido.')
+if not caminho_pasta.parent.is_dir(): 
+    print(f'{caminho_pasta.parent} não é um caminho válido.')
+else:
+    caminho_pasta.mkdir()
+    (caminho_pasta / '_README.md').touch()
+    (caminho_pasta / 'main.py').touch()
+    (caminho_pasta / '__init__.py').touch()
+    pasta_testes = caminho_pasta / 'tests'
+    pasta_testes.mkdir()
+    (pasta_testes/ '__init__.py').touch()
+    (pasta_testes/ 'test_.py').touch()
+    
