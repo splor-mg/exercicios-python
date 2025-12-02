76da84d38952144bbad8f339cd5d04e9eafab78d
Raiane Cardoso <166438302+raianecardoso@users.noreply.github.com>
2025-12-02 18:04:31 +0000

    correção 001

--- exercicios/01_essencial/soma_03_05/main.py
+++ exercicios/01_essencial/soma_03_05/main.py
@@ -1,3 +1,8 @@
 def resposta(n):
-    pass
+    somatorio = 0
+    while n > 0: 
+        if (n - 1) % 3 == 0 or (n - 1) % 5 == 0:
+            somatorio = somatorio + (n - 1)
+        n = n - 1            
+    return somatorio
 
