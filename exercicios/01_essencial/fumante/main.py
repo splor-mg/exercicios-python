4a38e76ff354ebdb8a7ea164cc57e047dc510a6c
Raiane Cardoso <166438302+raianecardoso@users.noreply.github.com>
2026-03-24 17:51:53 +0000

    Correção 001.

--- exercicios/01_essencial/fumante/main.py
+++ exercicios/01_essencial/fumante/main.py
@@ -1,2 +1,5 @@
 def resposta(cigarros, anos):
-    pass
+    total_cigarros = int(anos) * 365 * int(cigarros)
+    minutos = total_cigarros*10
+    dias = minutos / (60 * 24)
+    return (int(dias))
