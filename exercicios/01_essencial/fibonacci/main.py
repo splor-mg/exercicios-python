7a6dcce1c7fed4b34d8ed419d5dae7a1310286fb
raianecardoso <raianecardoso.rede@gmail.com>
2026-04-17 17:10:48 -0300

    Correção 001.

--- exercicios/01_essencial/fibonacci/main.py
+++ exercicios/01_essencial/fibonacci/main.py
@@ -1,2 +1,12 @@
 def resposta(n):
-    pass
+    a, b = 1, 1
+    k = 1
+    if n == 1:
+        return 1
+    elif n == 2:
+        return 1
+    else:
+        while k <= n-2:
+            a, b = b, a + b
+            k = k + 1
+        return b
