d5c6cd0c69af9660c03262c868bcaa63fc16cfcc
raianecardoso <raianecardoso.rede@gmail.com>
2026-04-17 16:58:43 -0300

    Correção 001.

--- exercicios/01_essencial/salario_descontos/main.py
+++ exercicios/01_essencial/salario_descontos/main.py
@@ -1,2 +1,8 @@
 def resposta(valor, horas):
-    pass
+    salario_bruto = valor * horas
+    imposto_renda = salario_bruto * 0.11
+    imposto_inss = salario_bruto * 0.08
+    sindicato = salario_bruto * 0.05
+    descontos = imposto_renda + imposto_inss + sindicato
+    salario_liquido = salario_bruto - descontos
+    return (salario_bruto, imposto_renda, imposto_inss, sindicato, salario_liquido)
