8c0b3bf5cdba1d561c94046ade3c69c7233c1d2c
raianecardoso <raianecardoso.rede@gmail.com>
2026-04-17 17:12:14 -0300

    Correção 001.

--- exercicios/01_essencial/divisor_comum/main.py
+++ exercicios/01_essencial/divisor_comum/main.py
@@ -1,2 +1,8 @@
-def resposta():
-    pass
+def resposta(numero1, numero2):
+    while numero2 != 0:
+        resultado = numero2
+        numero2 = numero1 % numero2
+        numero1 = numero2
+        if numero2 != 0:
+            resultado = numero2
+    return(resultado
