fe950523cbe8d6eb9539864e3c79a7d9309b9699
raianecardoso <raianecardoso.rede@gmail.com>
2026-04-17 16:56:22 -0300

    Correção 001.

--- exercicios/01_essencial/triangulo/main.py
+++ exercicios/01_essencial/triangulo/main.py
@@ -1,2 +1,10 @@
-def resposta():
-    pass
+def resposta(x, y, z):
+    if (x + y) <= z or (x + z) <= y or (y + z <= x):
+        return "não forma triângulo"
+    else:
+        if x == y and x == z:
+            return "equilátero"
+        elif x == y or x == z or y == z:
+            return "isósceles"
+        else:
+            return "escaleno"
