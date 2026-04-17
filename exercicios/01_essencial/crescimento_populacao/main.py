4e5c3a3b302b9118022b4cf0035ae34862a56855
raianecardoso <raianecardoso.rede@gmail.com>
2026-04-17 16:55:03 -0300

    Correção 001.

--- exercicios/01_essencial/crescimento_populacao/main.py
+++ exercicios/01_essencial/crescimento_populacao/main.py
@@ -1,2 +1,10 @@
 def resposta():
-    pass
+    a = 80000
+    b = 200000
+    anos = 0
+
+    while a <= b:
+        a = a + a * 0.03
+        b = b + b * 0.015
+        anos = anos + 1
+    return(anos)
