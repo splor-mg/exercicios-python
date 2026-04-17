16a5cf0d5b43c105d80be60d4a48ab09ea5146a4
raianecardoso <raianecardoso.rede@gmail.com>
2026-04-17 17:16:42 -0300

    Correção 001.

--- exercicios/01_essencial/maior_menor/main.py
+++ exercicios/01_essencial/maior_menor/main.py
@@ -1,2 +1,13 @@
-def resposta():
-    pass
+def resposta(n1, n2, n3):
+    if n1 >= n2:
+        maior_numero = n1
+        menor_numero = n2
+    else:
+        maior_numero = n2
+        menor_numero = n1
+    if maior_numero <= n3:
+        maior_numero = n3
+    if menor_numero >= n3:
+        menor_numero = n3
+    return (maior_numero, menor_numero)
+
