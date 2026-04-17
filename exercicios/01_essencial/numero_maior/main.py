e802affa43aeeb946e42814179c5d6f60ff92fc0
raianecardoso <raianecardoso.rede@gmail.com>
2026-04-17 17:17:51 -0300

    Correção 001.

--- exercicios/01_essencial/numero_maior/main.py
+++ exercicios/01_essencial/numero_maior/main.py
@@ -1,2 +1,8 @@
 def resposta(n1, n2, n3):
-    pass
+    if n1 >= n2 and n1 >= n3:
+        maior_numero = n1
+    elif n2 >= n3 and n2 >= n1:
+        maior_numero = n2
+    else:
+        maior_numero = n3
+    return maior_numero
