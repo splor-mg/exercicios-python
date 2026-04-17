4bef7ee3567f37b722215df585611865c7a11f2c
raianecardoso <raianecardoso.rede@gmail.com>
2026-04-17 17:18:52 -0300

    Correção 001.

--- exercicios/01_essencial/temperatura/main.py
+++ exercicios/01_essencial/temperatura/main.py
@@ -1,2 +1,6 @@
-def resposta():
-    pass
+def resposta(temperatura, tipo):
+    if tipo == "F":
+        temperatura = (temperatura - 32) * 5 / 9
+    else:
+        temperatura = (9 * temperatura / 5) + 32
+    return temperatura
