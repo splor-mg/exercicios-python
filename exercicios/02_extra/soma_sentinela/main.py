1a7023cedf775ee028102bd9f7652fac78b16c34
LorenadeCastro <lorenacrg24@gmail.com>
2026-07-13 16:09:24 -0300

    Correção 001.

--- exercicios/02_extra/soma_sentinela/main.py
+++ exercicios/02_extra/soma_sentinela/main.py
@@ -1,2 +1,12 @@
 def resposta(numeros):
-    pass
+    soma = 0
+    cont = 0
+
+    while cont < len(numeros):
+        if numeros[cont] == 0:
+            break
+
+        soma += numeros[cont]
+        cont += 1
+
+    return soma
